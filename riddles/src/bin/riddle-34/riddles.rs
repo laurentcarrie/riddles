019d60bef92197be9b745ac8d05db3378cba5894
Laurent Carrié <laurent.carrie@gmail.com>
2026-01-05 14:03:05 +0100

    ok

--- riddles/src/bin/riddle-34/riddles.rs
+++ riddles/src/bin/riddle-34/riddles.rs
@@ -28,14 +28,6 @@ pub fn remove_product_single_pairs(cg: &CurrentGuess) -> CurrentGuess {
     cg
 }
 
-pub fn all_possible_sums(cg: &CurrentGuess) -> HashSet<u32> {
-    let mut sums: HashSet<u32> = HashSet::new();
-    for (a, b) in cg.data.iter() {
-        sums.insert(a + b);
-    }
-    sums
-}
-
 pub fn complement_guess(cg: &CurrentGuess, n: u32) -> CurrentGuess {
     let mut ret = initial(n);
     for p in cg.data.iter() {
@@ -43,38 +35,6 @@ pub fn complement_guess(cg: &CurrentGuess, n: u32) -> CurrentGuess {
         ret.data.retain(|x| x != p);
     }
     ret
-}
-
-pub fn sum_of_two_primes(n: u32) -> HashMap<u32, Vec<(u32, u32)>> {
-    let mut sums: HashMap<u32, Vec<(u32, u32)>> = HashMap::new();
-    for i in 2..n {
-        if !is_prime(i) {
-            continue;
-        }
-        for j in i..n {
-            if !is_prime(j) {
-                continue;
-            }
-            let sum = i + j;
-            if let std::collections::hash_map::Entry::Vacant(e) = sums.entry(sum) {
-                e.insert(vec![(i, j)]);
-            } else {
-                sums.get_mut(&sum).unwrap().push((i, j));
-            }
-        }
-    }
-    sums
-}
-
-pub fn product_of_two_numbers(n: u32) -> HashMap<u32, Vec<(u32, u32)>> {
-    let mut products: HashMap<u32, Vec<(u32, u32)>> = HashMap::new();
-    for i in 2..n {
-        for j in i..n {
-            let product = i * j;
-            products.entry(product).or_default().push((i, j));
-        }
-    }
-    products
 }
 
 pub fn guess_sums(cg: CurrentGuess) -> HashSet<u32> {
